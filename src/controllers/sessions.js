35d09a3c85255d59352dbbc012c1648e30d6f662
Sorawit Suriyakarn <sorawit@mit.edu>
2015-11-01 08:31:41 -0500

    login and register returning many things.

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -32,14 +32,16 @@ module.exports = (app, io) => {
         req.body.display_name = req.body.display_name || (name[0].toUpperCase() + name.substr(1));
         req.body.display_img = req.body.display_img || user.picture.thumbnail;
         app.db(
-          'INSERT INTO users (username, password, display_name, display_img) VALUES ($1, $2, $3, $4) RETURNING id',
+          'INSERT INTO users (username, password, display_name, display_img) VALUES ($1, $2, $3, $4) RETURNING id, username, display_name, display_img',
           [req.body.username, req.body.password, req.body.display_name, req.body.display_img],
 
           (err, result) => {
             if (err) return res.status(400).end();
+            console.log(result);
             const token = app.sign(result[0].id);
             res.cookie(app.key, token);
-            res.json({ token: token, id: result[0].id });
+            result[0].token = token;
+            res.json(result[0]);
           }
         );
       });
@@ -48,7 +50,7 @@ module.exports = (app, io) => {
 
   app.post('/login', (req, res) => {
     app.db(
-      'SELECT id FROM users WHERE username = $1 AND password = $2',
+      'SELECT id, username, display_name, display_img FROM users WHERE username = $1 AND password = $2',
       [req.body.username, req.body.password],
 
       (err, result) => {
@@ -56,7 +58,8 @@ module.exports = (app, io) => {
         if (result.length === 0) return res.status(403).end();
         const token = app.sign(result[0].id);
         res.cookie(app.key, token);
-        res.json({ token: token, id: result[0].id });
+        result[0].token = token;
+        res.json(result[0]);
       }
     );
   });
